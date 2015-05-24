ef26e79297fd5a4741252042d137ea127c132323
Pierre JACQUIER <pierremtb@pc37.home>
2015-05-24 21:48:49 +0200

    Added scroll animation to material

--- templates/material/js/script.js
+++ templates/material/js/script.js
@@ -411,28 +411,28 @@ function movePresentation(pos_to_go)
     switch(pos_to_go)
        {
            case 0:
-                document.body.scrollTop = pos[0];
+                smooth_scroll_to(document.body, pos[0], 600);
                 document.getElementById("FAB_pres").style.opacity = 1;
                 document.getElementById('header').style.background = "transparent";
                 break;
            case 1:
-                document.body.scrollTop = pos[1];
+                smooth_scroll_to(document.body, pos[1], 600);
                 document.getElementById('header').style.background = "gray";
                 document.getElementById("FAB_pres").style.opacity = 0;
                 break;
            case 2:
-                document.body.scrollTop = pos[2];
+                smooth_scroll_to(document.body, pos[2], 600);
                 document.getElementById('header').style.background = "transparent";
                 document.getElementById("FAB_pres").style.opacity = 0;
                 break;
            case -1:
-                document.body.scrollTop = pos[2];
+                smooth_scroll_to(document.body, pos[2], 600);
                 document.getElementById('header').style.background = "transparent";
                 document.getElementById("FAB_pres").style.opacity = 0;  
                current_pos = 2;
                break;
             case 3:
-                document.body.scrollTop = pos[0];
+                smooth_scroll_to(document.body, pos[0], 600);
                 document.getElementById('header').style.background = "transparent";
                 document.getElementById("FAB_pres").style.opacity = 1;  
                current_pos = 0;
@@ -485,10 +485,13 @@ function detectSwipe(el,func) {
 
 function findSwipeDirection(el,d)
 {
-    if(d=="u")
-         movePresentation(current_pos + 1);
-    if(d=="d")
-         movePresentation(current_pos - 1);
+    if(current_tab == 1)
+    {
+        if(d=="u")
+             movePresentation(current_pos + 1);
+        if(d=="d")
+             movePresentation(current_pos - 1);
+    }
     if(d=="l")
     {
         switch(current_tab)
@@ -522,3 +525,71 @@ function findSwipeDirection(el,d)
 }
 
 detectSwipe("body",findSwipeDirection);
+
+var smooth_scroll_to = function(element, target, duration) {
+    target = Math.round(target);
+    duration = Math.round(duration);
+    if (duration < 0) {
+        return Promise.reject("bad duration");
+    }
+    if (duration === 0) {
+        element.scrollTop = target;
+        return Promise.resolve();
+    }
+
+    var start_time = Date.now();
+    var end_time = start_time + duration;
+
+    var start_top = element.scrollTop;
+    var distance = target - start_top;
+
+    // based on http://en.wikipedia.org/wiki/Smoothstep
+    var smooth_step = function(start, end, point) {
+        if(point <= start) { return 0; }
+        if(point >= end) { return 1; }
+        var x = (point - start) / (end - start); // interpolation
+        return x*x*(3 - 2*x);
+    }
+
+    return new Promise(function(resolve, reject) {
+        // This is to keep track of where the element's scrollTop is
+        // supposed to be, based on what we're doing
+        var previous_top = element.scrollTop;
+
+        // This is like a think function from a game loop
+        var scroll_frame = function() {
+            if(element.scrollTop != previous_top) {
+                reject("interrupted");
+                return;
+            }
+
+            // set the scrollTop for this frame
+            var now = Date.now();
+            var point = smooth_step(start_time, end_time, now);
+            var frameTop = Math.round(start_top + (distance * point));
+            element.scrollTop = frameTop;
+
+            // check if we're done!
+            if(now >= end_time) {
+                resolve();
+                return;
+            }
+
+            // If we were supposed to scroll but didn't, then we
+            // probably hit the limit, so consider it done; not
+            // interrupted.
+            if(element.scrollTop === previous_top
+                && element.scrollTop !== frameTop) {
+                resolve();
+                return;
+            }
+            previous_top = element.scrollTop;
+
+            // schedule next frame for execution
+            setTimeout(scroll_frame, 0);
+        }
+
+        // boostrap the animation process
+        setTimeout(scroll_frame, 0);
+    });
+}
