6f9fb619740e1843b684c01ec04f21a69db5b13f
Pierre JACQUIER <pierremtb@localhost.localdomain>
2015-06-01 21:14:38 +0200

    add gesture on sober

--- templates/sober/js/script.js
+++ templates/sober/js/script.js
@@ -28,6 +28,7 @@ if((""+window.location+"") == root + "#news")
 if((""+window.location+"") == root + "#more")
     setVisibleNotVisibleNotVisible(3,2,1)
 initialize();
+detectSwipe("body",findSwipeDirection);
 
 //Fonctions
 function initialize() {
@@ -135,4 +136,56 @@ function closeDrawer()
     drawer_opened = false;
         document.getElementById('shadow').style.zIndex = -10;
      document.getElementById('shadow').style.opacity = 0;
+}
+
+function detectSwipe(el,func) {
+      swipe_det = new Object();
+      swipe_det.sX = 0;
+      swipe_det.sY = 0;
+      swipe_det.eX = 0;
+      swipe_det.eY = 0;
+      var min_x = 200;  //min x swipe for horizontal swipe
+      var max_x = 300;  //max x difference for vertical swipe
+      var min_y = 140;  //min y swipe for vertical swipe
+      var max_y = 180;  //max y difference for horizontal swipe
+      var direc = "";
+      ele = document.body;
+      ele.addEventListener('touchstart',function(e){
+        var t = e.touches[0];
+        swipe_det.sX = t.screenX; 
+        swipe_det.sY = t.screenY;
+      },false);
+      ele.addEventListener('touchmove',function(e){
+       
+        var t = e.touches[0];
+        swipe_det.eX = t.screenX; 
+        swipe_det.eY = t.screenY;    
+      },false);
+      ele.addEventListener('touchend',function(e){
+        //horizontal detection
+        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
+          if(swipe_det.eX > swipe_det.sX) direc = "r";
+          else direc = "l";
+        }
+        //vertical detection
+        if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
+          if(swipe_det.eY > swipe_det.sY) direc = "d";
+          else direc = "u";
+        }
+    
+        if (direc != "") {
+          if(typeof func == 'function') func(el,direc);
+        }
+        direc = "";
+      },false);  
+}
+
+
+function findSwipeDirection(el,d)
+{
+
+    if(d=="r")
+    {
+        openDrawer();  
+    }
 }
